84b34d207d0ffa1b963adbb979b484417c8953d0
Artyom Pizin <artempetr2016@gmail.com>
2024-12-19 01:27:58 +0300

    feat: prohibit the user from entering more than 8 digits

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-const App = () => (
-  <label>
-    Input (a binary number): <input type="number" />
-  </label>
-);
+import { ChangeEvent, useState } from 'react';
+
+const App = () => {
+  const [binaryNumber, setBinaryNumber] = useState('0');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+
+    if (newValue.length <= 8) {
+      setBinaryNumber(newValue);
+    }
+  };
+
+  return (
+    <label>
+      Input (a binary number):{' '}
+      <input type="number" value={binaryNumber} onChange={handleInputChange} />
+    </label>
+  );
+};
 
 export default App;
