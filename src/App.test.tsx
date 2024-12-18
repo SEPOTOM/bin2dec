cbcde7dd12b713e403092972570a7b15da66f9a0
Artyom Pizin <artempetr2016@gmail.com>
2024-12-19 02:24:34 +0300

    test: add a test to display an error msg when entering an invalid digit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -24,4 +24,12 @@ describe('App', () => {
       screen.getByRole('spinbutton', { name: /input/i }),
     ).toHaveDisplayValue('1'.repeat(8));
   });
+
+  it('should notify the user if anything other than a 0 or 1 was entered', async () => {
+    const { user } = renderWithUser(<App />);
+
+    await user.type(screen.getByRole('spinbutton', { name: /input/i }), '5');
+
+    expect(screen.getByRole('alert')).toBeInTheDocument();
+  });
 });
