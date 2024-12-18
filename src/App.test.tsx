0ae4c4036d6c96a9b6bc6f0acf42bf489cb7ff53
Artyom Pizin <artempetr2016@gmail.com>
2024-12-19 01:13:16 +0300

    test: add a test to limit the user in the number of characters entered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 
 import App from '@/App';
+import { renderWithUser } from '@/tests';
 
 describe('App', () => {
   it('should display an input field', () => {
@@ -9,5 +10,18 @@ describe('App', () => {
     expect(
       screen.getByRole('spinbutton', { name: /input/i }),
     ).toBeInTheDocument();
+  });
+
+  it('should prohibit the user from entering more than 8 digits', async () => {
+    const { user } = renderWithUser(<App />);
+
+    await user.type(
+      screen.getByRole('spinbutton', { name: /input/i }),
+      '1'.repeat(9),
+    );
+
+    expect(
+      screen.getByRole('spinbutton', { name: /input/i }),
+    ).toHaveDisplayValue('1'.repeat(8));
   });
 });
