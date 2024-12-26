f4f5847b391a69d478fb9f27c1102b49c970e5d0
Artyom Pizin <artempetr2016@gmail.com>
2024-12-26 18:49:02 +0300

    fix(formatNumber): don't add a space at the start of numbers whose length is a multiple of 3

--- src/utils/formatters/formatters.ts
+++ src/utils/formatters/formatters.ts
@@ -10,7 +10,7 @@ export const formatNumber = (num: number): string => {
   let result = numStr.slice(0, startIndex);
 
   for (let i = startIndex; i < numStr.length; i += 1) {
-    if (i % 3 === startIndex) {
+    if (i % 3 === startIndex && i !== 0) {
       result += ` ${numStr[i]}`;
     } else {
       result += numStr[i];
