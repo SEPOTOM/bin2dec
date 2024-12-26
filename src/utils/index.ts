e2a5564f156f9bdf80b2fb31beaa497a4357acd3
Artyom Pizin <artempetr2016@gmail.com>
2024-12-26 18:47:30 +0300

    test: add more tests for the formatNumber util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { convertBinaryToDecimal } from '@/utils/converters';
-import { formatNumber } from '@/utils/formatters';
+import { formatNumber } from '@/utils/formatters/formatters';
 import { isBinaryStr } from '@/utils/strings';
 
 export { convertBinaryToDecimal, formatNumber, isBinaryStr };
